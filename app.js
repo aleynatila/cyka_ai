397f19093a548eb8a0caf553e161cd7e15769658
aleynatila <32612260+aleynatila@users.noreply.github.com>
2024-07-04 17:50:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const cookieParser = require('cookie-parser');
 dotenv.config({ path: './.env' });
 
 const app = express();
-
+/*
 const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
-});
+});*/
 
 app.use(session({
     secret: 'secret', // Ensure a long random string for production
